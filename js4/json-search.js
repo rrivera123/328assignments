6ad5e4a25e1fe2966129a09b38622bc5f386a2c1
Ryan Rivera <rrivera@mail.greenriver.edu>
2021-02-02 18:37:50 -0500

    Added people object containing data of 5 people to json-search.js

---
+++ js4/json-search.js
@@ -0,0 +1,48 @@
+let people = {
+    "People" : [
+        {"Name" : "Jimmy Hendricks",
+            "Sex" : "m",
+            "Born" : "11/27/1942",
+            "Died" : "09/18/1970",
+            "Father" : "James Hendricks",
+            "Mother" : "Lucille Jeter"
+        },
+
+        {
+            "Name" : "Ronny James Padavona (Dio)",
+            "Sex" : "m",
+            "Born" : "07/10/1942",
+            "Died" : "05/16/1910",
+            "Father" : "Pat Padavona",
+            "Mother" : "Anna Padavona"
+        },
+
+        {
+            "Name" : "Farrokh Bulsara (Freddie Mercury)",
+            "Sex" : "m",
+            "Born" : "09/05/1946",
+            "Died" : "11/24/1991",
+            "Father" : "Bomi Bulsara",
+            "Mother" : "Jer Bulsara"
+        },
+
+        {
+            "Name" : "Jane Doe",
+            "Sex" : "f",
+            "Born" : "1876",
+            "Died" : "1959",
+            "Father" : "Petreus de Milliano",
+            "Mother" : "Sophia van Damme"
+        },
+
+        {
+            "Name" : "Jane Ramirez",
+            "Sex" : "f",
+            "Born" : "1102",
+            "Died" : "--",
+            "Father" : "Themistocles Merovech",
+            "Mother" : "Amara Floros"
+        }
+
+]}
+
